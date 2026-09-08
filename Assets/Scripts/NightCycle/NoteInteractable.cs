fc04616340fb0d2124fa372a2b46f935d117f098
DIMASHARDBASS111 <ikgnnn000@gmail.com>
2026-09-08 23:58:47 +0300

    crown light smooth reveal fix + notes sound on interaction fix

--- Assets/Scripts/NightCycle/NoteInteractable.cs
+++ Assets/Scripts/NightCycle/NoteInteractable.cs
@@ -1,5 +1,8 @@
+using Core;
+using FMODUnity;
 using UnityEngine;
 using UnityEngine.Events;
+using Zenject;
 
 namespace NightCycle
 {
@@ -12,6 +15,17 @@ namespace NightCycle
 
         public bool trigger_once = true;
         public UnityEvent noteEvent;
+        public EventReference interact_sound;
+        public bool trigger_sound_once = false;
+        private bool soundFlag = true;
+
+        private AudioService _audioService;
+
+        [Inject]
+        public void Constructor(AudioService audioService)
+        {
+            _audioService = audioService;
+        }
 
         public void SetEvent(bool trigger)
         {
@@ -20,6 +34,14 @@ namespace NightCycle
 
         public void OnEnterFocus()
         {
+            if (soundFlag)
+            {
+                _audioService.PlayFMODEvent(interact_sound, transform.position);
+                if (trigger_sound_once)
+                {
+                    soundFlag = false;
+                }
+            }
             NoteController.Instance.ShowNote(this);
         }
 
